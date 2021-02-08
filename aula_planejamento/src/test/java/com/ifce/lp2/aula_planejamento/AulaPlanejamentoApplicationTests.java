e13a9c1e4eb361f8c9efe260feb31635a5a375ca
Eduardo304 <ed538031@gmail.com>
2021-02-08 15:27:14 -0300

    Teste commit1

---
+++ aula_planejamento/src/test/java/com/ifce/lp2/aula_planejamento/AulaPlanejamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifce.lp2.aula_planejamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaPlanejamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
